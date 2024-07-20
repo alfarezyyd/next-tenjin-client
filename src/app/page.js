4bbbc7acd7d58def92ec6ecec8ea0281649c9723
alfarezyyd <adityaalfarezyd@gmail.com>
2024-07-20 21:55:35 +0700

    feat: update

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,7 @@
 "use client"
-import dynamic from 'next/dynamic'
 
 import React from "react";
-import Header from "@/components/landing/Sections/Header";
 
-import Services from "@/components/landing/Sections/Services";
-import Projects from "@/components/landing/Sections/Projects";
-import Blog from "@/components/landing/Sections/Blog";
-import Pricing from "@/components/landing/Sections/Pricing";
-import Contact from "@/components/landing/Sections/Contact";
-import LandingLayout from "@/components/landing/LandingLayout";
 // Sections
 
 export default function Page() {
@@ -17,14 +9,7 @@ export default function Page() {
 
   return (
     <>
-      <LandingLayout>
-        <Header/>
-        <Services/>
-        <Projects/>
-        <Blog/>
-        <Pricing/>
-        <Contact/>
-      </LandingLayout>
+
     </>
   );
 }
