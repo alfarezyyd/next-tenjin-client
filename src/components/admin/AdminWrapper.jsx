7d7414a58f8e25ab740f9a94a3dcade33fdd848a
alfarezyyd <adityaalfarezyd@gmail.com>
2024-08-17 16:28:55 +0700

    feat: import nice scroll

--- src/components/admin/AdminWrapper.jsx
+++ src/components/admin/AdminWrapper.jsx
@@ -11,37 +11,36 @@ export default function AdminWrapper({children}) {
 
   useEffect(() => {
     const loadAssets = async () => {
-
-
       const jQueryModule = await import('jquery');
       window.jQuery = jQueryModule.default; // Also assign jQuery to window object
       await import('jquery-ui-dist/jquery-ui.min');
       await import('popper.js/dist/popper');
       await import('bootstrap/dist/js/bootstrap');
-      await import('nicescroll/dist/jquery.nicescroll.min');
       await import('moment/moment');
       await import('tooltip.js/dist/tooltip.min');
-
-
+      await import('jquery.nicescroll/dist/jquery.nicescroll.min');
     };
 
     if (typeof window !== "undefined") {
       loadAssets();
+      setLoading(false)
     }
   });
 
 
   return (
-    <div id="app">
-      <div className="main-wrapper">
-        <div className="navbar-bg"></div>
-        <AdminNavbar/>
-        <AdminSidebar/>
-        <div className="main-content">
-          {children}
+    <>
+      <div id="app">
+        <div className="main-wrapper">
+          <div className="navbar-bg"></div>
+          <AdminNavbar/>
+          <AdminSidebar/>
+          <div className="main-content">
+            {children}
+          </div>
+          <AdminFooter/>
         </div>
-        <AdminFooter/>
       </div>
-    </div>
+    </>
   );
 }
