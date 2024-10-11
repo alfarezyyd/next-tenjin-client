d09a0e8b626ff43d327e3bcfa9cb27947d977396
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-11 22:35:58 +0700

    feat: homepage

--- src/components/auth/AuthWrapper.jsx
+++ src/components/auth/AuthWrapper.jsx
@@ -10,7 +10,7 @@ export default function AuthWrapper({children}) {
             <div
               className="mx-auto flex min-h-full w-full flex-col justify-start pt-12 md:max-w-[75%] lg:h-screen lg:max-w-[1013px] lg:px-8 lg:pt-0 xl:h-[100vh] xl:max-w-[1383px] xl:px-0 xl:pl-[70px]">
               <div className="mb-auto flex flex-col pl-5 pr-5 md:pr-0 md:pl-12 lg:max-w-[48%] lg:pl-0 xl:max-w-full">
-                <Link to="/admin" className="mt-0 w-max lg:pt-10" href={"/admin"}>
+                <Link to="/" className="mt-0 w-max lg:pt-10" href={"/"}>
                   <div className="mx-auto flex h-fit w-fit items-center hover:cursor-pointer">
                     <svg
                       width="8"
@@ -25,7 +25,7 @@ export default function AuthWrapper({children}) {
                       />
                     </svg>
                     <p className="ml-3 text-sm text-gray-600">
-                      Back to Dashboard
+                      Back to Homepage
                     </p>
                   </div>
                 </Link>
