dc1175d55995480dbdf91d091b952f11dae598c3
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-12 20:57:08 +0700

    feat: order

--- src/app/admin/orders/page.jsx
+++ src/app/admin/orders/page.jsx
@@ -8,11 +8,27 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 import {Loading} from "@/components/admin/Loading";
 import Image from "next/image";
 
+
 export default function Page() {
   const [accessToken, setAccessToken] = useState(null);
   const [allOrder, setAllOrder] = useState({});
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    // You can also change below url value to any script url you wish to load,
+    // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
+    const midtransScriptUrl = 'https://app.sandbox.midtrans.com/snap/snap.js';
+
+    let scriptTag = document.createElement('script');
+    scriptTag.src = midtransScriptUrl;
+
+    // Optional: set script attribute, for example snap.js have data-client-key attribute
+    // (change the value according to your client-key)
+    const myMidtransClientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY;
+    scriptTag.setAttribute('data-client-key', myMidtransClientKey);
+    setLoading(false)
+    document.body.appendChild(scriptTag);
+
     async function loadAssets() {
       const $ = (await import('jquery')).default;
       await CommonStyle();
@@ -24,8 +40,19 @@ export default function Page() {
     }
     console.log(Cookies.get("accessToken"))
     setAccessToken(Cookies.get("accessToken"));
+    return () => {
+      document.body.removeChild(scriptTag);
+    }
   }, []);
 
+
+  async function initiatePayment(snapToken) {
+    window.snap.pay(snapToken, {
+      onClose: () => {
+        alert('Your payment is postponed, you can continue this order next time');
+      }
+    });
+  }
 
   useEffect(() => {
     fetchAllOrder()
@@ -54,61 +81,6 @@ export default function Page() {
         setLoading(false); // Menghentikan loading ketika data sudah diterima
       }
     }
-
-    return (
-      <AdminWrapper>
-        <section className="section">
-          <div className="section-header">
-            <h1>Order Assistensi</h1>
-            <div className="section-header-breadcrumb">
-              <div className="breadcrumb-item active"><a href="#">Admin</a></div>
-              <div className="breadcrumb-item"><a href="#">Mentor</a></div>
-              <div className="breadcrumb-item">Pengalaman</div>
-            </div>
-          </div>
-
-          <div className="section-body">
-            <div className="row">
-              {loading ? (  // Tampilkan loading selama data belum tersedia
-                <Loading/>
-              ) : (
-                allOrder.length > 0 ? (
-                  <div className="col-12 col-md-12 col-lg-12">
-                    <article className="article article-style-c d-flex flex-row">
-                      <Image src={`/images/authentication/login.jpg`} alt=""
-                             className="w-100 h-100 object-cover" width={500}
-                             height={500}/>
-                      <div className="article-details">
-                        <div className="article-category"><a href="#">News</a>
-                          <div className="bullet"></div>
-                          <a href="#">5 Days</a></div>
-                        <div className="article-title">
-                          <h2><a href="#">Excepteur sint occaecat cupidatat non proident</a></h2>
-                        </div>
-                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                          cillum dolore eu fugiat nulla pariatur. </p>
-                        <div className="article-user">
-                          <img alt="image" src="../assets/img/avatar/avatar-1.png"/>
-                          <div className="article-user-details">
-                            <div className="user-detail-name">
-                              <a href="#">Hasan Basri</a>
-                            </div>
-                            <div className="text-job">Web Developer</div>
-                          </div>
-                        </div>
-                      </div>
-                    </article>
-                  </div>
-                ) : (
-                  <Loading/>
-                )
-              )
-              }
-            </div>
-          </div>
-        </section>
-      </AdminWrapper>
-    );
   }
 
   return (
@@ -158,7 +130,11 @@ export default function Page() {
                               </div>
                               <div className="text-job">Web Developer</div>
                             </div>
-                            <a href="#" className="btn btn-outline-primary float-right mt-1">Primary</a>
+                            <a href="#" className="btn btn-outline-primary float-right mt-1 ml-2">Cancel</a>
+                            <a href="#" onClick={() => {
+                              initiatePayment(mentorAssistance['id'])
+                            }}
+                               className="btn  btn-primary float-right mt-1">Continue</a>
                           </div>
                         </div>
                       </div>
