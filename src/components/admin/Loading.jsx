2b64cd74adc23c5d9a6230fefb199254327bbd59
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-10 20:50:14 +0700

    feat: loading

--- src/components/admin/Loading.jsx
+++ src/components/admin/Loading.jsx
@@ -1,17 +1,15 @@
 export const Loading = () => {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center fixed inset-0">
-      <div className="lds-grid">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+  return (<div className="w-screen h-screen flex justify-center items-center fixed inset-0">
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
-  )
+  </div>)
 }
