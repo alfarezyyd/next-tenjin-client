8d8e33d455a7132cf667b22c6bb5cd0339ad1c69
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-24 18:51:37 +0700

    feat: update config

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  images: {
-    remotePatterns: [{
-      protocol: 'http', hostname: 'localhost', port: '3001', // Tambahkan port yang sesuai
-      pathname: '**',
-    },],
-  },
-};
--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,15 @@ import CaseSensitivePathsPlugin from "case-sensitive-paths-webpack-plugin";
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
-      ...config.resolve.alias,
-      'jquery': path.resolve('node_modules/jquery/dist/jquery'),
-      'jQuery': 'jquery',
+      ...config.resolve.alias, 'jquery': path.resolve('node_modules/jquery/dist/jquery'), 'jQuery': 'jquery',
     };
     config.plugins.push(new CaseSensitivePathsPlugin());
     return config;
+  }, images: {
+    remotePatterns: [{
+      protocol: 'http', hostname: 'localhost', port: '3001', // Tambahkan port yang sesuai
+      pathname: '**',
+    },],
   },
 };
 
