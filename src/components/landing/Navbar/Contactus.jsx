9e381ad79f7b12b6ba74d5d137fb027ca12b54cb
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-28 00:21:08 +0700

    fix: also check is expired

--- src/components/landing/Navbar/Contactus.jsx
+++ src/components/landing/Navbar/Contactus.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import {useEffect, useState} from "react";
 import Cookies from "js-cookie";
+import {CommonUtil} from "@/common/utils/common-util";
 
 
 const Contactusform = () => {
@@ -13,10 +14,12 @@ const Contactusform = () => {
   return (
     <div className=" inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto md:ml-6 sm:pr-0 ">
       <div className='hidden lg:block'>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}${accessToken ? 'admin/dashboard' : 'auth/login'}`} passHref>
+        <Link
+          href={`${process.env.NEXT_PUBLIC_BASE_URL}${accessToken && !CommonUtil.isTokenExpired(accessToken) ? 'admin/dashboard' : 'auth/login'}`}
+          passHref>
           <button type="button"
                   className='justify-end text-xl font-semibold bg-transparent py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-navyblue hover:text-white'>
-            {accessToken ? 'Admin' : 'Get Started'}
+            {accessToken && !CommonUtil.isTokenExpired(accessToken) ? 'User Area' : 'Get Started'}
           </button>
         </Link>
       </div>
