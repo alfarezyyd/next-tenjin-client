76ebd4e908001fe9319155f238107d39ddaac370
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-28 12:55:18 +0700

    fix: import dropdown first

--- src/components/admin/CommonScript.jsx
+++ src/components/admin/CommonScript.jsx
@@ -1,6 +1,7 @@
 export default async function CommonScript() {
   await import('jquery.nicescroll/jquery.nicescroll');
   await import('nicescroll/jquery.nicescroll')
+  await import('bootstrap/js/dist/dropdown'); // Pastikan ini jalan lebih dulu
   await import('../../../public/assets/js/scripts');
   await import('../../../public/assets/js/custom');
 }
